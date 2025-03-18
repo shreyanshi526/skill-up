9f74303b3508632034f9eea37bd212213e939e51
Shreyanshi261 <shreyanshi.kushwaha@zevo360.com>
2025-03-18 18:20:12 +0530

    feat(api): changed show mentors feature

--- Server/Services/chat.service.ts
+++ Server/Services/chat.service.ts
@@ -38,7 +38,17 @@ export const getRecommendedMentors = async (currentUserId: string, res: Response
         return res.status(400).json({ message: "User Id is required." });
     }
 
-    const users = await Users.find({ _id: { $ne: currentUserId } });
+    const users = await Users.aggregate([
+        { $match: { _id: { $ne: currentUserId } } }, // Exclude current user
+        {
+          $group: {
+            _id: null, // Single object output
+            in_network: { $push: { $cond: { if: { $ifNull: ["$chatHistory", false] }, then: "$$ROOT", else: "$$REMOVE" } } },
+            outside_network: { $push: { $cond: { if: { $ifNull: ["$chatHistory", false] }, then: "$$REMOVE", else: "$$ROOT" } } }
+          }
+        },
+      ]);
+      
 
     return users;
 };
